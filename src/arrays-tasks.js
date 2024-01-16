4a2f1e2efa0fc6514d92e0ef975489f8aba04df6
Andrey Gateluyk <gatelyuk97@mail.ru>
2024-01-16 22:19:48 +0300

    complite findElement, findAllOccurrences

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -66,8 +66,8 @@ function sumArrays(arr1, arr2) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
@@ -84,8 +84,14 @@ function findElement(/* arr, value */) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  const count = 0;
+  return arr.reduce((acc, elem) => {
+    if (elem === item) {
+      return acc + 1;
+    }
+    return acc;
+  }, count);
 }
 
 /**
